7cc4dd3bcac54304ddd9973493994f95197e329b
glauslivio <eugen-yevheniy@yandex.com>
2019-04-29 09:24:26 -0700

    Init Testapp to test plugin working

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,71 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var demoUrl = "https://academy.zenva.com/wp-json/zva-mobile-app/v1/demoWidevine";
+var paused = false;
+var is_downloading = false;
+var streaming_url = "https://zavideoplatform.streaming.mediaservices.windows.net///2c856c2a-b469-4a0d-a6c1-984fee7017c9/03. Creating Numpy Arrays.ism/manifest(format=mpd-time-csf,encryption=cenc)";
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FjYWRlbXkuemVudmEuY29tIiwiaWF0IjoxNTU2MzYzNjQxLCJleHAiOjE1NTYzNzgwNzEsImF1ZCI6Imh0dHBzOi8vYWNhZGVteS56ZW52YS5jb20iLCJzdWIiOjAsInp2YWlwIjoiMTA0LjIwMy4xNTUuOTciLCJ6dmFwb3N0IjowfQ.cM-KrQpcD4U3Mkabvzw5f-UFLHGasPfmau94VYSr5pU";
+var ccUrl = "https://zavideoplatform.blob.core.windows.net/closed-captions/lesson-635112-en.vtt";
+var lessonId = "ZenvaTest2";
+
+var setListener = function() {
+    $("#stream_video").click(function(){
+        window.plugins.Bitmovin.streamVideo(streaming_url, token, ccUrl, 10);
+    });
+
+    $("#download_video").click(function(){
+        is_downloading = true;
+        $("#delete_downlaodvideo").attr("disabled", false);
+        $("#pause_resume_download").attr("disabled", false);
+        window.plugins.Bitmovin.downloadVideo(streaming_url, token, ccUrl, lessonId, 540,
+            function(data){
+                console.log(data);
+                if(data.event == "progress"){
+                    $("#myBar").width(data.value + '%');
+                    $("#myValue").html(data.value + '%');
+                }
+                if(data.event == "complete"){
+                    is_downloading = false;
+                    $("#pause_resume_download").attr("disabled", true);
+                    $("#play_downloadvideo").attr("disabled", false);
+                }
+                if(data.event == "error"){
+                    
+                }
+            },
+            function(err){
+                console.error(err);
+            }
+        );
+    });
+
+   $("#pause_resume_download").click(function(){
+        if(!is_downloading){
+            return false;
+        }
+        paused = !paused;
+        if(paused){
+            window.plugins.Bitmovin.pauseDownload(lessonId);
+            $("#pause_resume_download").html("Resume");
+        }else{
+            window.plugins.Bitmovin.resumeDownload(lessonId);
+            $("#pause_resume_download").html("Pause");
+        }
+    });
+
+    $("#play_downloadvideo").click(function(){
+        window.plugins.Bitmovin.playDownloadedVideo(lessonId, 0);
+    });
+
+    $("#delete_downlaodvideo").click(function(){
+        window.plugins.Bitmovin.deleteDownloadedVideo(lessonId);
+        $("#play_downloadvideo").attr("disabled", true);
+        $("#delete_downlaodvideo").attr("disabled", true);
+    });
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -27,19 +92,12 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        $.get( demoUrl, function( data ) {
+          streaming_url = data.streamingLocatorUrlWidevine;
+          token = data.tokenWidevine;
+          ccUrl = data.ccUrl;
+          setListener();
+        });
     }
 };
 
